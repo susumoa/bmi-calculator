8a9d4aa4de949c8623c4769360543cd6352048c5
susumoa <tobiasbori@gmail.com>
2020-07-03 14:01:08 +0100

    button calculates bmi and clear form

--- src/components/BmiInput.js
+++ src/components/BmiInput.js
@@ -1,22 +1,39 @@
 import React, {useState} from 'react'
 
-const [measurements, setMeasurements] = useState({
-  date: '',
-  height: '',
-  weight: ''
-})
-
-const {date, height, weight} = measurements
-
-const onChange = event => {
-  const {name, value} = event.target
-  setMeasurements(prevMeasurements => {
-    return {...prevMeasurements,
-    [name]: value}
-  })
-}
-
 const BmiInput = () => {
+  const [measurements, setMeasurements] = useState({
+    date: '',
+    height: '',
+    weight: ''
+  })
+  
+  const [bmi, setBmi] = useState('')
+  
+  const {date, height, weight} = measurements
+  
+  const onChange = event => {
+    const {name, value} = event.target
+    setMeasurements(prevMeasurements => {
+      return {...prevMeasurements,
+      [name]: value}
+    })
+  }
+  
+  const calculateBmi = event => {
+    event.preventDefault()
+    if (height === '' || weight === '' || date === '') {
+      alert('Please fill out everything')
+    } else {
+      console.log('clicked')
+      setBmi((weight / ((height / 100) * (height / 100))).toFixed(1))
+      setMeasurements({
+        height: '',
+        weight: '',
+        date: ''
+      })
+    }
+  }
+
   return (
     <div>
       <h1>Daily measurements</h1>
@@ -34,6 +51,7 @@ const BmiInput = () => {
         <br />
         <button onClick={calculateBmi}>Calculate</button>
       </form>
+      {bmi !== '' && <p>BMI: {bmi}</p>}
     </div>
   )
 }
