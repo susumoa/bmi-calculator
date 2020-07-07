e39d79feff4b2d8795d0a89e165065eeee341ebd
susumoa <tobiasbori@gmail.com>
2020-07-07 10:47:46 +0100

    create chart component

---
+++ src/components/Chart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Chart } from "react-google-charts";
+
+const Component = () => {
+  return (
+    <div>
+      Chart
+    </div>
+  )
+}
+
+export default Component
