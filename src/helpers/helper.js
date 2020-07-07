1e59dc22058b9cd2ffceb8380b0b7719b348325e
susumoa <tobiasbori@gmail.com>
2020-07-07 11:17:06 +0100

    change helper classes to helper arrays for chart

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,56 +1,71 @@
 // age group 16-24
-const group1624 = {
-  underweight: 5,
-  normal: 57,
-  overweight: 22,
-  obese: 16
-}
+const group1624 = [
+  ['Weight Range', 'Rates'],
+  ['underweight', 5],
+  ['normal', 57],
+  ['overweight', 22],
+  ['obese', 16]
+]
 
-const group2534 = {
-  underweight: 2,
-  normal: 41,
-  overweight: 34,
-  obese: 23
-}
+// age group 25-34
+const group2534 = [
+  ['Weight Range', 'Rates'],
+  ['underweight', 2],
+  ['normal', 41],
+  ['overweight', 34],
+  ['obese', 23]
+]
 
-const group3544 = {
-  underweight: 1,
-  normal: 33,
-  overweight: 37,
-  obese: 28
-}
+// age group 35-44
+const group3544 = [
+  ['Weight Range', 'Rates'],
+  ['underweight', 1],
+  ['normal', 33],
+  ['overweight', 37],
+  ['obese', 29]
+]
 
-const group4554 = {
-  underweight: 1,
-  normal: 27,
-  overweight: 36,
-  obese: 36
-}
+// age group 45-54
+const group4554 = [
+  ['Weight Range', 'Rates'],
+  ['underweight', 1],
+  ['normal', 27],
+  ['overweight', 36],
+  ['obese', 36]
+]
 
-const group5564 = {
-  underweight: 0,
-  normal: 27,
-  overweight: 39,
-  obese: 34
-}
+// age group 55-64
+const group5564 = [
+  ['Weight Range', 'Rates'],
+  ['underweight', 0],
+  ['normal', 27],
+  ['overweight', 39],
+  ['obese', 34]
+]
 
-const group6574 = {
-  underweight: 1,
-  normal: 24,
-  overweight: 43,
-  obese: 32
-}
+// age group 65-74
+const group6574 = [
+  ['Weight Range', 'Rates'],
+  ['underweight', 1],
+  ['normal', 24],
+  ['overweight', 43],
+  ['obese', 32]
+]
 
-const group75 = {
-  underweight: 1,
-  normal: 27,
-  overweight: 41,
-  obese: 31
-}
+// age group 75+
+const group75 = [
+  ['Weight Range', 'Rates'],
+  ['underweight', 1],
+  ['normal', 27],
+  ['overweight', 41],
+  ['obese', 31]
+]
 
-const total = {
-  underweight: 2,
-  normal: 34,
-  overweight: 36,
-  obese: 28
-}
+// total
+const total = [
+  ['Weight Range', 'Rates'],
+  ['underweight', 2],
+  ['normal', 34],
+  ['overweight', 36],
+  ['obese', 28]
+]
