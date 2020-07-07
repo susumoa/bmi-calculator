757eca5cd3497cb34e414e15c36dbba813eaa31f
susumoa <tobiasbori@gmail.com>
2020-07-07 10:28:53 +0100

    add age group helper classes with adult bmi data

---
+++ src/helpers/helper.js
@@ -0,0 +1,56 @@
+// age group 16-24
+const group1624 = {
+  underweight: 5,
+  normal: 57,
+  overweight: 22,
+  obese: 16
+}
+
+const group2534 = {
+  underweight: 2,
+  normal: 41,
+  overweight: 34,
+  obese: 23
+}
+
+const group3544 = {
+  underweight: 1,
+  normal: 33,
+  overweight: 37,
+  obese: 28
+}
+
+const group4554 = {
+  underweight: 1,
+  normal: 27,
+  overweight: 36,
+  obese: 36
+}
+
+const group5564 = {
+  underweight: 0,
+  normal: 27,
+  overweight: 39,
+  obese: 34
+}
+
+const group6574 = {
+  underweight: 1,
+  normal: 24,
+  overweight: 43,
+  obese: 32
+}
+
+const group75 = {
+  underweight: 1,
+  normal: 27,
+  overweight: 41,
+  obese: 31
+}
+
+const total = {
+  underweight: 2,
+  normal: 34,
+  overweight: 36,
+  obese: 28
+}
